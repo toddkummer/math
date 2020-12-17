19519cdb8bdcdd39c9b851f331c583271a91f9b5
Todd Kummer <todd@rockridgesolutions.com>
2020-12-17 12:27:26 -0800

    Add prettier (via eslint)

--- docs/scripts/equations.js
+++ docs/scripts/equations.js
@@ -1,38 +1,53 @@
 class Equation {
-  constructor (a, b) {
+  constructor(a, b) {
     this.a = a
     this.b = b
   }
 
-  get display () {
+  get display() {
     return `${this.a} ${this.constructor.operator} ${this.b} = ${this.answer}`
   }
 }
 
 class Addition extends Equation {
-    static operator = '+'
-    get answer () { return this.a + this.b }
+  static operator = '+'
+
+  get answer() {
+    return this.a + this.b
+  }
 }
 
 class Subtraction extends Equation {
-    static operator = '-'
-    get answer () { return this.a - this.b }
+  static operator = '-'
+  get answer() {
+    return this.a - this.b
+  }
 }
 
 class Multiplication extends Equation {
-    static operator = '*'
-    get answer () { return this.a * this.b }
+  static operator = '*'
+  get answer() {
+    return this.a * this.b
+  }
 }
 
 class Division extends Equation {
-    static operator = '+'
-    get answer () { return this.a / this.b }
+  static operator = '+'
+  get answer() {
+    return this.a / this.b
+  }
 }
 
 class RemainderDivision extends Equation {
-  get answer () { return Math.floor(this.a / this.b) }
-  get remainder () { return this.a % this.b }
-  get display () {
+  get answer() {
+    return Math.floor(this.a / this.b)
+  }
+
+  get remainder() {
+    return this.a % this.b
+  }
+
+  get display() {
     return `${this.a} ${this.constructor.operator} ${this.b} = ${this.answer}`
   }
 }
