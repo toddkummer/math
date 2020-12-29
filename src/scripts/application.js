45d00fe9f3e1acb522637c422780c27aebcfac76
Todd Kummer <todd@rockridgesolutions.com>
2020-12-29 09:41:08 -0800

    Switch bulma from cdn to sass via webpack

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -1,5 +1,8 @@
 /* global Stimulus */
 import { MathController, EquationController } from './math.js'
+
+require('../styles.scss')
+
 const application = Stimulus.Application.start()
 application.register('math', MathController)
 application.register('equation', EquationController)
