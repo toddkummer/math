3d4ffc8de3616439be09981369bda9dcb2a2afa8
Todd Kummer <todd@rockridgesolutions.com>
2020-12-29 10:21:21 -0800

    Switch font awesome to webpack
    The applicaiton.js file has the imports commented out. Put them back in when an icon is needed.

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -1,8 +1,13 @@
 /* global Stimulus */
 import { MathController, EquationController } from './math.js'
+// import { library, dom } from '@fortawesome/fontawesome-svg-core'
+// import { faEquals } from '@fortawesome/free-solid-svg-icons'
 
 require('../styles.scss')
 
 const application = Stimulus.Application.start()
 application.register('math', MathController)
 application.register('equation', EquationController)
+
+// library.add(faEquals)
+// dom.watch()
