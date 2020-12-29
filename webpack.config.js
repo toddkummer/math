02dccd192dc0d0bb32035c5cac809fa7f279c8f3
Todd Kummer <todd@rockridgesolutions.com>
2020-12-28 17:16:14 -0800

    Add webpack config for html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/scripts/application.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'application.js',
     path: path.resolve(__dirname, 'docs'),
