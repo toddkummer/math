8bb63454831d9e6268ca5b5fd169a4674054d1c4
Todd Kummer <todd@rockridgesolutions.com>
2020-12-29 20:58:15 -0800

    Update webpack config with clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
     application: './src/scripts/application.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: true,
