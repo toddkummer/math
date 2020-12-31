0235d5ae7c73a09f05bac95a36d58ddf4f321781
Todd Kummer <todd@rockridgesolutions.com>
2020-12-30 16:03:11 -0800

    Extract class for Quiz Controller

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -1,15 +1,17 @@
 import { Application } from 'stimulus-repo/packages/stimulus'
 import { MathController } from './math_controller.js'
+import { PracticeController } from './practice_controller'
+import { QuizController } from './quiz_controller'
 import { EquationController } from './equation_controller'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faAppleAlt, faPencilRuler } from '@fortawesome/free-solid-svg-icons'
-import { PracticeController } from './practice_controller'
 
 require('../styles.scss')
 
 const application = Application.start()
 application.register('math', MathController)
 application.register('practice', PracticeController)
+application.register('quiz', QuizController)
 application.register('equation', EquationController)
 
 library.add(faAppleAlt, faPencilRuler)
---
+++ src/scripts/quiz_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from 'stimulus-repo/packages/stimulus'
+
+export class QuizController extends Controller {
+  static targets = ['questions', 'submit', 'results', 'restart']
+  static parent = 'math'
+  static children = ['equation']
+
+  show() {
+    this.element.classList.remove('is-hidden')
+  }
+
+  hide() {
+    this.element.classList.add('is-hidden')
+  }
+
+  submit() {
+    console.log('Test has finished')
+    let correct = 0
+    this.equationChildren.forEach((equation) => {
+      if (equation.correct()) {
+        equation.right()
+        correct++
+      } else {
+        equation.wrong()
+      }
+    })
+    this.submitTarget.classList.replace('is-primary', 'is-static')
+    this.restartTarget.classList.remove('is-hidden')
+    this.displayResults(correct)
+  }
+
+  displayResults(correct) {
+    const total = this.equationChildren.length
+    this.resultsTarget.classList.remove('is-hidden')
+    this.resultsTarget.textContent = `You got ${correct} out of ${total} correct!`
+  }
+
+  restart() {
+    this.equationChildren.forEach((equation) => equation.reset())
+    this.submitTarget.classList.replace('is-static', 'is-primary')
+    this.resultsTarget.classList.add('is-hidden')
+    this.restartTarget.classList.add('is-hidden')
+  }
+
+  generateQuestions() {
+    if (!this.questionsGenerated) {
+      for (let counter = 1; counter < 20; counter++) {
+        const clone = this.equationChild.element.cloneNode(true)
+        this.questionsTarget.appendChild(clone)
+      }
+      this.questionsGenerated = true
+    }
+  }
+}
