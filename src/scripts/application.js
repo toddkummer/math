f234b7237c547610d2bfca0d128b4689643f0e98
Todd Kummer <todd@rockridgesolutions.com>
2021-01-03 13:10:36 -0800

    Navigation Bar: Handle burger event, add title, center

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -5,6 +5,7 @@ import { QuizController } from './quiz_controller'
 import { EquationController } from './equation_controller'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faAppleAlt, faPencilRuler } from '@fortawesome/free-solid-svg-icons'
+import { NavigationBarController } from './navigation_bar_controller'
 
 require('../styles.scss')
 
@@ -13,6 +14,7 @@ application.register('math', MathController)
 application.register('practice', PracticeController)
 application.register('quiz', QuizController)
 application.register('equation', EquationController)
+application.register('navigation-bar', NavigationBarController)
 
 library.add(faAppleAlt, faPencilRuler)
 dom.watch()
